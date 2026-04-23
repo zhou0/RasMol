3d302c3cb31a2dc9d104e74d834a30101e0b7ff5
zhou0 <5291860+zhou0@users.noreply.github.com>
2026-04-23 10:51:50 +0000

    Fix CI build: add HDF5 dependency for CBFlib
    - Updated GitHub Actions workflow to install libhdf5-dev on Ubuntu.
    - Updated CMakeLists.txt to find and include HDF5 directories, which are required by the CBFlib headers.
    - Handled fallback for HDF5 headers in standard locations.

---
+++ build/CMakeFiles/rastxt.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/app/src/abstree.c" "CMakeFiles/rastxt.dir/src/abstree.c.o" "gcc" "CMakeFiles/rastxt.dir/src/abstree.c.o.d"
+  "/app/src/cif.c" "CMakeFiles/rastxt.dir/src/cif.c.o" "gcc" "CMakeFiles/rastxt.dir/src/cif.c.o.d"
+  "/app/src/cif_ctonum.c" "CMakeFiles/rastxt.dir/src/cif_ctonum.c.o" "gcc" "CMakeFiles/rastxt.dir/src/cif_ctonum.c.o.d"
+  "/app/src/cif_fract.c" "CMakeFiles/rastxt.dir/src/cif_fract.c.o" "gcc" "CMakeFiles/rastxt.dir/src/cif_fract.c.o.d"
+  "/app/src/cif_stx.c" "CMakeFiles/rastxt.dir/src/cif_stx.c.o" "gcc" "CMakeFiles/rastxt.dir/src/cif_stx.c.o.d"
+  "/app/src/cmndline.c" "CMakeFiles/rastxt.dir/src/cmndline.c.o" "gcc" "CMakeFiles/rastxt.dir/src/cmndline.c.o.d"
+  "/app/src/command.c" "CMakeFiles/rastxt.dir/src/command.c.o" "gcc" "CMakeFiles/rastxt.dir/src/command.c.o.d"
+  "/app/src/infile.c" "CMakeFiles/rastxt.dir/src/infile.c.o" "gcc" "CMakeFiles/rastxt.dir/src/infile.c.o.d"
+  "/app/src/langsel_unix.c" "CMakeFiles/rastxt.dir/src/langsel_unix.c.o" "gcc" "CMakeFiles/rastxt.dir/src/langsel_unix.c.o.d"
+  "/app/src/maps.c" "CMakeFiles/rastxt.dir/src/maps.c.o" "gcc" "CMakeFiles/rastxt.dir/src/maps.c.o.d"
+  "/app/src/molecule.c" "CMakeFiles/rastxt.dir/src/molecule.c.o" "gcc" "CMakeFiles/rastxt.dir/src/molecule.c.o.d"
+  "/app/src/multiple.c" "CMakeFiles/rastxt.dir/src/multiple.c.o" "gcc" "CMakeFiles/rastxt.dir/src/multiple.c.o.d"
+  "/app/src/outfile.c" "CMakeFiles/rastxt.dir/src/outfile.c.o" "gcc" "CMakeFiles/rastxt.dir/src/outfile.c.o.d"
+  "/app/src/pixutils.c" "CMakeFiles/rastxt.dir/src/pixutils.c.o" "gcc" "CMakeFiles/rastxt.dir/src/pixutils.c.o.d"
+  "/app/src/rastxt.c" "CMakeFiles/rastxt.dir/src/rastxt.c.o" "gcc" "CMakeFiles/rastxt.dir/src/rastxt.c.o.d"
+  "/app/src/render.c" "CMakeFiles/rastxt.dir/src/render.c.o" "gcc" "CMakeFiles/rastxt.dir/src/render.c.o.d"
+  "/app/src/repres.c" "CMakeFiles/rastxt.dir/src/repres.c.o" "gcc" "CMakeFiles/rastxt.dir/src/repres.c.o.d"
+  "/app/src/script.c" "CMakeFiles/rastxt.dir/src/script.c.o" "gcc" "CMakeFiles/rastxt.dir/src/script.c.o.d"
+  "/app/src/tokens.c" "CMakeFiles/rastxt.dir/src/tokens.c.o" "gcc" "CMakeFiles/rastxt.dir/src/tokens.c.o.d"
+  "/app/src/transfor.c" "CMakeFiles/rastxt.dir/src/transfor.c.o" "gcc" "CMakeFiles/rastxt.dir/src/transfor.c.o.d"
+  "/app/src/vector.c" "CMakeFiles/rastxt.dir/src/vector.c.o" "gcc" "CMakeFiles/rastxt.dir/src/vector.c.o.d"
+  "/app/src/wbrotate.c" "CMakeFiles/rastxt.dir/src/wbrotate.c.o" "gcc" "CMakeFiles/rastxt.dir/src/wbrotate.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
