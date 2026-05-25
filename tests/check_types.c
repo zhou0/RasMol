bde5ae1b142cd3dba63d8d6490fa4296a2cf4658
zhou0 <zhou0@users.noreply.github.com>
2026-05-25 12:51:15 +0800

    Delete tests/check_types.c

--- tests/check_types.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdint.h>
-#include "src/rasmol.h"
-int main() {
-    printf("sizeof(Pixel): %zu\n", sizeof(Pixel));
-    printf("sizeof(Long): %zu\n", sizeof(Long));
-    printf("sizeof(int): %zu\n", sizeof(int));
-    return 0;
-}
